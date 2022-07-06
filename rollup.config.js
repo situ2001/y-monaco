1f63975e754014931386331e7cfbcc7be8030293
situ2001 <yongcong2001@outlook.com>
2022-07-06 19:10:10 +0800

    Revert "update dependencies and make this an esm module"
    This reverts commit ae2d46d6dcc4e863d8836768ff4be2459573bce5.

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,65 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve'
-// import commonjs from '@rollup/plugin-commonjs'
+import commonjs from '@rollup/plugin-commonjs'
 import postcss from 'rollup-plugin-postcss'
 import fs from 'fs'
 import path from 'path'
 
 const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, './package.json'), { encoding: 'utf8' }))
 const dependencies = Object.keys(pkg.peerDependencies).concat(Object.keys(pkg.dependencies))
+
+const customModules = new Set([
+  'y-websocket',
+  'y-codemirror',
+  'y-ace',
+  'y-textarea',
+  'y-quill',
+  'y-dom',
+  'y-prosemirror'
+])
+/**
+ * @type {Set<any>}
+ */
+const customLibModules = new Set([
+  'lib0',
+  'y-protocols'
+])
+const debugResolve = {
+  resolveId (importee) {
+    if (importee === 'yjs/tests/testHelper.js') {
+      return `${process.cwd()}/../yjs/tests/testHelper.js`
+    }
+    if (importee === 'yjs') {
+      return `${process.cwd()}/../yjs/src/index.js`
+    }
+    if (importee === 'y-monaco') {
+      return `${process.cwd()}/src/y-monaco.js`
+    }
+    if (customModules.has(importee.split('/')[0])) {
+      return `${process.cwd()}/../${importee}/src/${importee}.js`
+    }
+    if (customLibModules.has(importee.split('/')[0])) {
+      return `${process.cwd()}/../${importee}`
+    }
+    if (importee === 'monaco-editor') {
+      return `${process.cwd()}/node_modules/monaco-editor/esm/vs/editor/editor.api.js`
+    }
+    return null
+  }
+}
+
 export default [{
   input: './src/y-monaco.js',
   output: [{
     name: 'Y',
-    file: 'dist/y-monaco.cjs',
+    file: 'dist/y-monaco.js',
     format: 'cjs',
-    sourcemap: true
+    sourcemap: true,
+    paths: path => {
+      if (/^lib0\//.test(path)) {
+        return `lib0/dist/${path.slice(5)}`
+      }
+      return path
+    }
   }],
   external: id => dependencies.some(dep => dep === id) || /^lib0\//.test(id)
 }, {
@@ -21,31 +68,25 @@ export default [{
     name: 'test',
     file: 'dist/test.cjs',
     format: 'cjs',
-    sourcemap: true,
-    inlineDynamicImports: true
+    sourcemap: true
   },
-  external: id => !/^yjs\//.test(id) && id[0] !== '.' && id[0] !== '/',
   plugins: [
-    // debugResolve,
-    nodeResolve({
-      mainFields: ['module', 'browser', 'main']
-    }),
+    debugResolve,
     postcss({
       plugins: [],
       extract: true
     })
   ]
-} /* {
+}, {
   input: './test/index.js',
   output: {
     name: 'test',
     file: 'dist/test.js',
-    format: 'esm',
-    sourcemap: true,
-    inlineDynamicImports: true
+    format: 'iife',
+    sourcemap: true
   },
   plugins: [
-    // debugResolve,
+    debugResolve,
     nodeResolve({
       mainFields: ['module', 'browser', 'main']
     }),
@@ -55,4 +96,4 @@ export default [{
       extract: true
     })
   ]
-} */]
+}]
